2facf323e669d012ad3eb66075e9eb9533ed5e85
iSperia <skapust88@gmail.com>
2021-03-06 03:51:43 +0300

    android immersive full screen

--- android/android/src/com/game7th/swipe/AndroidLauncher.kt
+++ android/android/src/com/game7th/swipe/AndroidLauncher.kt
@@ -1,13 +1,23 @@
 package com.game7th.swipe
 
 import android.os.Bundle
+import android.view.View
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val config = AndroidApplicationConfiguration()
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+        val config = AndroidApplicationConfiguration().apply {
+            useImmersiveMode = true
+        }
         initialize(SwipeGameGdx(AndroidStorage(applicationContext)), config)
     }
 }
