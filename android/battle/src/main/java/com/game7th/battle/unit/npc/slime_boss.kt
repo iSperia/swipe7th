9bf7fc17783f7e63bdcacc3b797e6b7221baea4f
iSperia <skapust88@gmail.com>
2021-03-29 11:36:08 +0300

    add some abilities for slime mother

--- android/battle/src/main/java/com/game7th/battle/unit/npc/slime_boss.kt
+++ android/battle/src/main/java/com/game7th/battle/unit/npc/slime_boss.kt
@@ -13,25 +13,46 @@ import com.game7th.battle.unit.UnitStats
 import com.game7th.metagame.dto.UnitType
 
 fun produceSlimeBoss(balance: SwipeBalance, level: Int): UnitStats {
-    val hp = balance.slime.hp * level
+    val hp = balance.slime_boss.hp * level
     val slime = UnitStats(UnitType.SLIME_BOSS, level = level, health = CappedStat(hp, hp))
     slime += ability {
         ticker {
-            bodies[TickerEntry(balance.slime_boss.w1, balance.slime.t1, "sword")] = { battle, unit ->
+            bodies[TickerEntry(balance.slime_boss.w1, balance.slime_boss.t1, "sword")] = { battle, unit ->
                 val damage = (unit.stats.level * balance.slime_boss.k1).toInt()
                 if (damage > 0) {
+                    //physical attack
                     battle.findClosestAliveEnemy(unit)?.let { target ->
                         battle.notifyAttack(unit, listOf(target), 0)
                         battle.processDamage(target, unit, DamageVector(damage, 0, 0))
                     }
                 }
+
+                //generate two nodes
+                (0..1).forEach { _ ->
+                    battle.tileField.calculateFreePosition()?.let { position ->
+                        val tile = SwipeTile(TileTemplate("slime_splash", 0), battle.tileField.newTileId(), balance.slime_boss.d1, true)
+                        battle.tileField.tiles[position] = tile
+                        battle.notifyEvent(BattleEvent.CreateTileEvent(tile.toViewModel(), position, -1))
+                    }
+                }
             }
             bodies[TickerEntry(balance.slime_boss.w2, balance.slime_boss.t2, "leaf")] = { battle, unit ->
-                battle.tileField.calculateFreePosition()?.let { position ->
-                    val tile = SwipeTile(TileTemplate("slime_splash", 0), battle.tileField.newTileId(), balance.slime_boss.d2, true)
-                    battle.tileField.tiles[position] = tile
-                    battle.notifyAttack(unit, emptyList(), 1)
-                    battle.notifyEvent(BattleEvent.CreateTileEvent(tile.toViewModel(), position, -1))
+                val damage = (unit.stats.level * balance.slime_boss.k2).toInt()
+                if (damage > 0) {
+                    //physical attack
+                    battle.aliveEnemies(unit).random().let { target ->
+                        battle.notifyAttack(unit, listOf(target), 1)
+                        battle.processDamage(target, unit, DamageVector(0, damage, 0))
+                    }
+                }
+
+                //generate two nodes
+                (0..2).forEach { _ ->
+                    battle.tileField.calculateFreePosition()?.let { position ->
+                        val tile = SwipeTile(TileTemplate("slime_splash", 0), battle.tileField.newTileId(), balance.slime_boss.d2, true)
+                        battle.tileField.tiles[position] = tile
+                        battle.notifyEvent(BattleEvent.CreateTileEvent(tile.toViewModel(), position, -1))
+                    }
                 }
             }
         }
