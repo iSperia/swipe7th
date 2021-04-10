94c4d7463d7e42e0de3d8d0ed1f7073060791709
iSperia <skapust88@gmail.com>
2021-04-11 02:11:42 +0300

    ios

--- android/ios/src/com/game7th/swipe/IOSLauncher.java
+++ android/ios/src/com/game7th/swipe/IOSLauncher.java
@@ -5,12 +5,34 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.game7th.metagame.PersistentStorage;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+    public static final String PREF_NAME = "com.game7th.swiped.prefs";
+
+    private class IosPersistentStorage implements PersistentStorage {
+
+        public IOSApplication app;
+
+        @Override
+        public void put(String key, String value) {
+            app.getPreferences(PREF_NAME).putString(key, value);
+        }
+
+        @Override
+        public String get(String key) {
+            return app.getPreferences(PREF_NAME).getString(key);
+        }
+    }
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new SwipeGameGdx(), config);
+
+        IosPersistentStorage storage = new IosPersistentStorage();
+        IOSApplication app = new IOSApplication(new SwipeGameGdx(storage), config);
+        storage.app = app;
+        return app;
     }
 
     public static void main(String[] argv) {
