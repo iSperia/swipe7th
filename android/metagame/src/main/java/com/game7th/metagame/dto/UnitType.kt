1375012a9b5db9b750a9aa0523d7ed503057569b
iSperia <skapust88@gmail.com>
2021-09-05 14:06:06 +0300

    add undead knight

--- android/metagame/src/main/java/com/game7th/metagame/dto/UnitType.kt
+++ android/metagame/src/main/java/com/game7th/metagame/dto/UnitType.kt
@@ -4,12 +4,14 @@ enum class UnitType(val bodyWeight: Int, val spiritWeight: Int, val mindWeight: 
     prince(2,2,2),
     stomper(0,0,0),
     arachnovisk(0, 0, 0),
+    undead_knight(0, 0, 0),
     UNKNOWN(0,0,0);
 
     fun getSkin(): String = when (this) {
         prince -> "prince"
         stomper -> "stomper"
         arachnovisk -> "arachnovisk"
+        undead_knight -> "undead_knight"
         else -> "personage_dead"
     }
 }
